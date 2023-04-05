112b8f92beac901f1cea216e79c1498897beebbb
Miguel Ridruejo <ridruejomiguel@gmail.com>
2023-04-05 13:25:07 +0200

    Initial Commit

---
+++ AppPruebaGIT/AppPruebaGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPruebaGITApp.swift
+//  AppPruebaGIT
+//
+//  Created by Miguel Ridruejo on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AppPruebaGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
